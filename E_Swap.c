d4364080a2914fe62356606ba5624ecad8fec075
Nasim <mdnasimhosen333@gmail.com>
2025-05-05 22:38:26 +0600

    Add implementation of E. Swap problem from Codeforces

---
+++ E_Swap.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+void swap(int *x, int *y)
+{
+    int temp = *x;
+    *x = *y;
+    *y = temp;
+}
+
+int main()
+{
+    int x, y;
+    scanf("%d %d", &x, &y);
+    swap(&x, &y);
+    printf("%d %d\n", x, y);
+
+    return 0;
+}
