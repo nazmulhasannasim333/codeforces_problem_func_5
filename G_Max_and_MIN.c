9af8d3fbb4ba39d4191f2139c174205eb89d0f2f
Nasim <mdnasimhosen333@gmail.com>
2025-05-07 21:13:47 +0600

    Add implementation of G. Max and MIN problem from Codeforces

---
+++ G_Max_and_MIN.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+void min_max(int arr[], int n)
+{
+    int min = arr[0], max = arr[0];
+    for (int i = 1; i < n; i++)
+    {
+        if (arr[i] < min)
+        {
+            min = arr[i];
+        }
+        if (arr[i] > max)
+        {
+            max = arr[i];
+        }
+    }
+    printf("%d %d\n", min, max);
+}
+
+int main()
+{
+    int n;
+    scanf("%d", &n);
+    int arr[n];
+    for (int i = 0; i < n; i++)
+    {
+        scanf("%d", &arr[i]);
+    }
+    min_max(arr, n);
+    return 0;
+}
